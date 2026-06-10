2169f37c20d93c19c2790de0f029dd4d97d02e68
Vedant Deokar <vedantd@rhythm.local>
2026-06-10 15:45:30 +0530

    tenant_id change

--- pages/registration/modules/customer/scripts/batch_create.py
+++ pages/registration/modules/customer/scripts/batch_create.py
@@ -24,7 +24,7 @@ from common.erp_api_client import RhythmERPAPIClient
 from common.logger import log
 from pages.registration.modules.customer.data.customer_data import generate_customer_api_payload
 
-DEFAULT_TENANT_ID = "599"
+DEFAULT_TENANT_ID = "681"
 DEFAULT_COUNT = 10
 
 
